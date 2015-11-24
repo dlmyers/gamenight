5c86d9920786d7307b948f1851d355a6ea55a41f
Don Myers <don.myers@apple.com>
2015-11-24 11:11:34 -0800

    add bootstrap styling, add fields to friends views, change date column for events from string to datetime

--- gamenight/db/schema.rb
+++ gamenight/db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101170327) do
+ActiveRecord::Schema.define(version: 20151124190912) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
-    t.string   "date"
+    t.datetime "date"
     t.string   "location"
     t.text     "description"
     t.datetime "created_at",  null: false
