03d6b6b9b93c79aa315e3c6093f0d81e267b10db
mario_cristiani <mcristiani@gmail.com>
2015-11-28 20:27:59 +0000

    Add Devise user auth

--- gamenight/app/models/event.rb
+++ gamenight/app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ActiveRecord::Base
 	has_many :invites
 	has_many :friends, through: :invites
+    belongs_to :users
     validates :name, :date, :location, presence: true
+    
+     # would need to validate timeliness for event date > https://github.com/adzap/validates_timeliness
+    
+    # do we need to get the current_user ID here  
 end
